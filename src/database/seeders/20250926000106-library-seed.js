f6df65cb580d4ca5d640946339b693acde2364ae
Gabrielmarlier3 <gabrielmarliere2005@gmail.com>
2025-09-29 18:34:37 -0300

    feat: add image URLs to library seed data for better media representation

--- src/database/seeders/20250926000106-library-seed.js
+++ src/database/seeders/20250926000106-library-seed.js
@@ -2,36 +2,47 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('library', [
-      {
-        user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1',
-        mal_id: 11061,
-        title: 'Hunter x Hunter (2011)',
-        media_type: 'series',
-        season: 'fall',
-        year: 2011,
-        status: 'Finished Airing',
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1',
-        mal_id: 21,
-        title: 'One Piece',
-        media_type: 'series',
-        season: 'fall',
-        year: 1999,
-        status: 'Currently Airing',
-        created_at: new Date(),
-        updated_at: new Date(),
-      }
-    ]);
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'library',
+      [
+        {
+          user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1',
+          mal_id: 11061,
+          title: 'Hunter x Hunter (2011)',
+          media_type: 'series',
+          season: 'fall',
+          image_url: 'https://cdn.myanimelist.net/images/anime/1337/99013.jpg',
+          year: 2011,
+          status: 'Finished Airing',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1',
+          mal_id: 21,
+          title: 'One Piece',
+          media_type: 'series',
+          season: 'fall',
+          image_url: 'https://cdn.myanimelist.net/images/anime/6/73245.jpg',
+          year: 1999,
+          status: 'Currently Airing',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('library', {
-      user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1'
-    }, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete(
+      'library',
+      {
+        user_uid: 'RyMIKpIe25ZFs0F6ibZLlIIkM2z1',
+        mal_id: { [Sequelize.Op.in]: [11061, 21] },
+      },
+      {},
+    );
+  },
 };
