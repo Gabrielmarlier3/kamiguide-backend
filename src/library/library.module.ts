9bf5282c3f2c31f7b20f89a07a8a9dc47eae2edd
Gabrielmarlier3 <gabrielmarliere2005@gmail.com>
2025-09-29 17:13:00 -0300

    feat: add Sequelize integration for LibraryModel in library module

--- src/library/library.module.ts
+++ src/library/library.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LibraryService } from './library.service';
 import { LibraryController } from './library.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { LibraryModel } from './library.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([LibraryModel])],
   providers: [LibraryService],
   controllers: [LibraryController],
 })
